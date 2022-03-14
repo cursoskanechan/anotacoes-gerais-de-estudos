45868fbb75dab20503e6c4148832180c52a1bef4
kanechan <kanes.chan@gmail.com>
2022-03-14 16:31:30 -0300

    integracao formulario
    Integração com o formulário.

---
+++ typescript-curso/dist/js/app.js
@@ -0,0 +1,7 @@
+import { NegociacaoController } from "./controllers/necociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
